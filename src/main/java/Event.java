1fdbf59e0932eb169ff6698b0025d4b4b1ceb79b
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-09-08 18:26:10 -0700

    create Event class with constructor and constructor test

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+class Event {
+
+  private int mGuestNumber;
+  private String mFoodType;
+  private String mDrinkType;
+  private String mEntertainmentType;
+
+  public Event(int guestNumber, String foodType, String drinkType, String entertainmentType) {
+    mGuestNumber = guestNumber;
+    mFoodType = foodType;
+    mDrinkType = drinkType;
+    mEntertainmentType = entertainmentType;
+  }
+  
+}
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,6 +1,11 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public Class EventTest {
-  
+public class EventTest {
+
+  @Test
+  public void Event_instantiatesCorrectly() {
+    Event testEvent = new Event(2, "food", "drink", "entertain");
+    assertEquals(true, testEvent instanceof Event);
+  }
 }
