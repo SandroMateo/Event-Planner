d2e45f04d99053d325ddbf3bb4cb2adbb4255a6d
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-09-09 08:42:55 -0700

    add a calculateCost method in Event class with corresponding test

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,36 +1,39 @@
 class Event {
 
-  private int mGuestNumber;
+  private double mGuestNumber;
   private float mCost;
-  private String mFoodType;
-  private String mFoodAmount;
+  private String mFoodCuisine;
+  private String mMealAmount;
   private String mMealType;
   private String mDrinkType;
   private String mEntertainmentType;
-  private int[] mCostArray = {10, 15, 20, 25, 30, 35};
-  private String[] mFoodTypeArray = {"American", "Mexican", "Indian", "Italian", "Japanese", "French"};
+  private double[] mCostArray = {10, 15, 20, 25, 30, 35};
+  private double[] mMealCostArray = {1, 1.5, 2, 0.75};
+  private String[] mFoodCuisineArray = {"American", "Mexican", "Indian", "Italian", "Japanese", "French"};
+  private String[] mMealAmountArray = {"Small", "Medium", "Large", "Light Snacks"};
+  private String[] mMealTypeArray = {"Vegan", "Vegeterian", "No Preference", ""};
   private String[] mDrinkArray = {"Soda Bar", "Juice Bar", "Soda and Juice Bar", "Assorted Local Beer", "Assorted Beer with Select Cocktails", "Full Bar"};
   private String[] mEntertainmentArray = {"Our Own Staff DJ", "A Local Struggling Comedian", "A Local Struggling Artist", "A Well Known Local Comedian", "A Well Known Local Band", "A Nationally Famous Band or Comedian"};
 
-  public Event(int guestNumber, String foodType, String foodAmount, String mealType, String drinkType, String entertainmentType) {
+  public Event(double guestNumber, String foodCuisine, String mealAmount, String mealType, String drinkType, String entertainmentType) {
     mGuestNumber = guestNumber;
-    mFoodType = foodType;
-    mFoodAmount = foodAmount;
+    mFoodCuisine = foodCuisine;
+    mMealAmount = mealAmount;
     mMealType = mealType;
     mDrinkType = drinkType;
     mEntertainmentType = entertainmentType;
   }
 
-  public int getGuestNumber() {
+  public double getGuestNumber() {
     return mGuestNumber;
   }
 
-  public String getFoodType() {
-    return mFoodType;
+  public String getFoodCuisine() {
+    return mFoodCuisine;
   }
 
-  public String getFoodAmount() {
-    return mFoodAmount;
+  public String getMealAmount() {
+    return mMealAmount;
   }
 
   public String getMealType() {
@@ -45,7 +48,30 @@ class Event {
     return mEntertainmentType;
   }
 
-  public float getCost() {
+  public double getCost() {
     return mCost;
+  }
+
+  public void calculateCost() {
+    for (int i = 0; i < mCostArray.length; i++) {
+      if(mFoodCuisine.equalsIgnoreCase(mFoodCuisineArray[i])) {
+        mCost += mCostArray[i];
+      }
+      if(mDrinkType.equalsIgnoreCase(mDrinkArray[i])) {
+        mCost += mCostArray[i];
+      }
+      if(mEntertainmentType.equalsIgnoreCase(mEntertainmentArray[i])) {
+        mCost += mCostArray[i];
+      }
+    }
+    for(int j = 0; j < mMealCostArray.length; j++) {
+      if(mMealAmount.equalsIgnoreCase(mMealAmountArray[j])) {
+        mCost *= mMealCostArray[j];
+      }
+      if(mMealType.equalsIgnoreCase(mMealTypeArray[j])) {
+        mCost *= mMealCostArray[j];
+      }
+    }
+    mCost *= mGuestNumber;
   }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -12,19 +12,19 @@ public class EventTest {
   @Test
   public void Event_getGuestNumber_2() {
     Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
-    assertEquals(2, testEvent.getGuestNumber());
+    assertEquals(2, testEvent.getGuestNumber(), 1);
   }
 
   @Test
-  public void Event_getFoodType_food() {
+  public void Event_getFoodCuisine_food() {
     Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
-    assertEquals("food", testEvent.getFoodType());
+    assertEquals("food", testEvent.getFoodCuisine());
   }
 
   @Test
-  public void Event_getFoodAmount_small() {
+  public void Event_getMealAmount_small() {
     Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
-    assertEquals("amount", testEvent.getFoodAmount());
+    assertEquals("amount", testEvent.getMealAmount());
   }
 
   @Test
@@ -49,5 +49,12 @@ public class EventTest {
   public void Event_getCost_0() {
     Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
     assertEquals(0, testEvent.getCost(), 1);
+  }
+
+  @Test
+  public void Event_calculateCost_120() {
+    Event testEvent = new Event(2, "American", "Small", "No preference", "Soda Bar", "Our Own Staff DJ");
+    testEvent.calculateCost();
+    assertEquals(120, testEvent.getCost(), 1);
   }
 }
