fe12950e8605f3393c2e76a72f9bdf126d96f4df
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-09-08 20:43:33 -0700

    add mFoodAmount, mMealType, mFoodTypeArray, mCostArray, mDrinkArray, and mEntertainmentArray to Event class with corresponding get methods and tests

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,22 @@
 class Event {
 
   private int mGuestNumber;
+  private float mCost;
   private String mFoodType;
+  private String mFoodAmount;
+  private String mMealType;
   private String mDrinkType;
   private String mEntertainmentType;
-  private int mCost;
+  private int[] mCostArray = {10, 15, 20, 25, 30, 35};
+  private String[] mFoodTypeArray = {"American", "Mexican", "Indian", "Italian", "Japanese", "French"};
+  private String[] mDrinkArray = {"Soda Bar", "Juice Bar", "Soda and Juice Bar", "Assorted Local Beer", "Assorted Beer with Select Cocktails", "Full Bar"};
+  private String[] mEntertainmentArray = {"Our Own Staff DJ", "A Local Struggling Comedian", "A Local Struggling Artist", "A Well Known Local Comedian", "A Well Known Local Band", "A Nationally Famous Band or Comedian"};
 
-  public Event(int guestNumber, String foodType, String drinkType, String entertainmentType) {
+  public Event(int guestNumber, String foodType, String foodAmount, String mealType, String drinkType, String entertainmentType) {
     mGuestNumber = guestNumber;
     mFoodType = foodType;
+    mFoodAmount = foodAmount;
+    mMealType = mealType;
     mDrinkType = drinkType;
     mEntertainmentType = entertainmentType;
   }
@@ -21,6 +29,14 @@ class Event {
     return mFoodType;
   }
 
+  public String getFoodAmount() {
+    return mFoodAmount;
+  }
+
+  public String getMealType() {
+    return mMealType;
+  }
+
   public String getDrinkType() {
     return mDrinkType;
   }
@@ -29,7 +45,7 @@ class Event {
     return mEntertainmentType;
   }
 
-  public int getCost() {
+  public float getCost() {
     return mCost;
   }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -5,37 +5,49 @@ public class EventTest {
 
   @Test
   public void Event_instantiatesCorrectly() {
-    Event testEvent = new Event(2, "food", "drink", "entertain");
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
     assertEquals(true, testEvent instanceof Event);
   }
 
   @Test
   public void Event_getGuestNumber_2() {
-    Event testEvent = new Event(2, "food", "drink", "entertain");
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
     assertEquals(2, testEvent.getGuestNumber());
   }
 
   @Test
   public void Event_getFoodType_food() {
-    Event testEvent = new Event(2, "food", "drink", "entertain");
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
     assertEquals("food", testEvent.getFoodType());
   }
 
   @Test
+  public void Event_getFoodAmount_small() {
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
+    assertEquals("amount", testEvent.getFoodAmount());
+  }
+
+  @Test
+  public void Event_getMealType_food() {
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
+    assertEquals("meal", testEvent.getMealType());
+  }
+
+  @Test
   public void Event_getDrinkType_drink() {
-    Event testEvent = new Event(2, "food", "drink", "entertain");
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
     assertEquals("drink", testEvent.getDrinkType());
   }
 
   @Test
   public void Event_getEntertainmentType_entertain() {
-    Event testEvent = new Event(2, "food", "drink", "entertain");
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
     assertEquals("entertain", testEvent.getEntertainmentType());
   }
 
   @Test
   public void Event_getCost_0() {
-    Event testEvent = new Event(2, "food", "drink", "entertain");
-    assertEquals(0, testEvent.getCost());
+    Event testEvent = new Event(2, "food", "amount", "meal", "drink", "entertain");
+    assertEquals(0, testEvent.getCost(), 1);
   }
 }
